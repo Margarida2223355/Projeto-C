ab6030025a67cdc363c091216418eb8b1494d0c6
Camacho071098 <maggie_fcamacho98@hotmail.com>
2022-12-15 20:33:36 +0000

    Revert "Alterações nas Estruturas de Dados"
    This reverts commit c88c0d46c423e3e97430d6e7ffec3069f6adb32b.

--- dados.c
+++ dados.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Definição das constantes */
 #define MAX_P 5000
 #define MAX_A 200
 #define MAX_I 10000
 #define MAX_CARACT 50
 
 
-/* Estruturas */
 typedef struct {
     int dia, mes, ano;
 } Data;
@@ -20,28 +18,27 @@ typedef struct {
 typedef struct {
     int id_part;
     long nif, telefone;
-    char nome[MAX_CARACT], email[MAX_CARACT];
-    int escola;
+    char nome[MAX_CARACT], email[MAX_CARACT], escola[5];
 } t_participante;
 
 typedef struct {
     int id_ativ;
-    char designacao[MAX_CARACT], local[MAX_CARACT];
-    int tipo_atividade, associacao;
+    char designacao[MAX_CARACT], tipo_atividade[MAX_CARACT], associacao[MAX_CARACT], local[MAX_CARACT];
     float valor_inscricao;
     Data data;
     Hora hora;
-} t_ativividade;
+} Atividade;
 
 typedef struct {
     int id_ins, id_ins_part, id_ins_ativ;
     float valor_ins_ativ;
     Data data;
     Hora hora;
-} t_inscricao;
+} Inscricao;
 
 
-/* Definir quantidade de participantes, atividades e inscrições */
+
 t_participante participante[MAX_P];
-t_ativividade atividade[MAX_A];
-t_inscricao inscricao[MAX_I];
+Atividade atividade[MAX_A];
+Inscricao inscricao[MAX_I];
+
