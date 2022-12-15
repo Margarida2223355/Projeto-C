c88c0d46c423e3e97430d6e7ffec3069f6adb32b
Camacho071098 <maggie_fcamacho98@hotmail.com>
2022-12-15 20:17:34 +0000

    Alterações nas Estruturas de Dados

--- dados.c
+++ dados.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Definição das constantes */
 #define MAX_P 5000
 #define MAX_A 200
 #define MAX_I 10000
 #define MAX_CARACT 50
 
 
+/* Estruturas */
 typedef struct {
     int dia, mes, ano;
 } Data;
@@ -18,27 +20,28 @@ typedef struct {
 typedef struct {
     int id_part;
     long nif, telefone;
-    char nome[MAX_CARACT], email[MAX_CARACT], escola[5];
+    char nome[MAX_CARACT], email[MAX_CARACT];
+    int escola;
 } t_participante;
 
 typedef struct {
     int id_ativ;
-    char designacao[MAX_CARACT], tipo_atividade[MAX_CARACT], associacao[MAX_CARACT], local[MAX_CARACT];
+    char designacao[MAX_CARACT], local[MAX_CARACT];
+    int tipo_atividade, associacao;
     float valor_inscricao;
     Data data;
     Hora hora;
-} Atividade;
+} t_ativividade;
 
 typedef struct {
     int id_ins, id_ins_part, id_ins_ativ;
     float valor_ins_ativ;
     Data data;
     Hora hora;
-} Inscricao;
+} t_inscricao;
 
 
-
+/* Definir quantidade de participantes, atividades e inscrições */
 t_participante participante[MAX_P];
-Atividade atividade[MAX_A];
-Inscricao inscricao[MAX_I];
-
+t_ativividade atividade[MAX_A];
+t_inscricao inscricao[MAX_I];
